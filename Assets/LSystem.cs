e8c138cac3d802a27aef7f9680a9d486433ae501
ScottSpiers <scott.m.spiers@gmail.com>
2019-05-15 14:46:05 +0100

    Initial Commit (Initial LSystem functionality)

---
+++ Assets/LSystem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSystem
+{
+    struct ProductionRule
+    {
+        public ProductionRule(char p, string s) { pre = p; suc = s; prob = 1.0f; }
+        public ProductionRule(char p, string s, float probability) { pre = p; suc = s; prob = probability; }
+        public char pre { get; }
+        public string suc { get; }
+        public float prob { get; }
+    };
+
+    private string axiom;
+    private List<ProductionRule> rules;
+
+    public LSystem()
+    {
+        axiom = "";
+        rules = new List<ProductionRule>();
+    }
+
+    public LSystem(string a) : this()
+    {        
+        axiom = a;
+    }
+
+    public void AddRule(char p, string s)
+    {
+        AddRule(p, s, 1.0f);
+    }
+
+    public void AddRule(char p, string s, float prob)
+    {
+        rules.Add(new ProductionRule(p, s, prob));
+    }
+
+    public string RunSystem(int iterations)
+    {
+        string nextWord = "";
+        string curWord = axiom;
+        bool replaced = false;
+
+        for(int i = 0; i < iterations; ++i)
+        {
+            foreach(char c in curWord)
+            {
+                replaced = false;
+                foreach(ProductionRule r in rules)
+                {
+                    if(c == r.pre)
+                    {
+                        if (ApplyRule(r.prob))
+                        {
+                            nextWord += r.suc;
+                            replaced = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!replaced)
+                    nextWord += c;
+            }
+
+            curWord = nextWord;
+            nextWord = "";
+        }
+
+        return curWord;
+    }
+
+    private bool ApplyRule(float prob)
+    {
+        float r = Random.Range(0.0f, 1.0f);
+        return r <= prob;
+    }
+}
---
+++ Assets/Tester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LSystem lSys = new LSystem("F");
+        lSys.AddRule('F', "FF");
+
+        Debug.Log(lSys.RunSystem(4));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
