8a19a40c2f9d3f06d8ee88086eed3caec6e1d88c
ScottSpiers <scott.m.spiers@gmail.com>
2019-05-22 13:36:27 +0100

    Implementing Parametric L-Systems

--- Assets/LSystem.cs
+++ Assets/LSystem.cs
@@ -1,30 +1,160 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
+
 
 public class LSystem
 {
+    public delegate float[] Transition(float[] args);
+    
+    public struct Module
+    {
+        public Module(char a) { sym = a; parameters = new List<float>(); trans = new Transition((float[] f) => { return f; }); }
+        public Module(char a, List<float> ps) { sym = a; parameters = ps; trans = new Transition((float[] f) => { return f; }); }
+        public Module(char a, List<float> ps, Transition t) { sym = a; parameters = ps; trans = t; }
+
+        public char sym { get; }
+        public List<float> parameters { get; set; }
+        public Transition trans { get; }
+
+        public void SetParams(List<float> ps)
+        {
+            parameters.Clear();
+            parameters.InsertRange(0, ps);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Module other = (Module)obj;
+            return (sym == other.sym) && (parameters.Count == other.parameters.Count);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string str_out = sym.ToString();
+            if(parameters.Count > 0)
+            {
+                str_out += "(";
+
+                foreach(float f in parameters)
+                {
+                    str_out += f;
+                    str_out += ",";
+                }
+
+                str_out = str_out.Remove(str_out.Length -1);
+                str_out += ")";
+            }                           
+            return str_out;
+        }
+    };
+
+
     struct ProductionRule
     {
-        public ProductionRule(char p, string s) { pre = p; suc = s; prob = 1.0f; }
-        public ProductionRule(char p, string s, float probability) { pre = p; suc = s; prob = probability; }
-        public char pre { get; }
-        public string suc { get; }
+        public ProductionRule(Module p, List<Module> s, float probability) { pre = p; suc = s; prob = probability;}
+
+        public Module pre { get; }
+        public List<Module> suc { get; }
+       
         public float prob { get; }
     };
 
-    private string axiom;
+    private List<Module> axiom;
     private List<ProductionRule> rules;
+    private Dictionary<char, float> varMap;
 
     public LSystem()
     {
-        axiom = "";
+        axiom = new List<Module>();
         rules = new List<ProductionRule>();
+        varMap = new Dictionary<char, float>();
     }
 
     public LSystem(string a) : this()
+    {
+        axiom = new List<Module>();
+
+        foreach(char c in a)
+        {
+            axiom.Add(new Module(c));
+        }
+    }
+
+    public LSystem(Module a) : this()
+    {
+        axiom.Add(a);
+    }
+
+    public LSystem(List<Module> a) : this()
     {        
         axiom = a;
+    }
+
+
+    public List<Module> RunSystem(int iterations)
+    {
+        List<Module> nextWord = new List<Module>();
+        List<Module> curWord = axiom;
+
+        for (int i = 0; i < iterations;  ++i)
+        {
+            foreach (Module m in curWord)
+            {
+                Debug.Log(m);
+                bool replaced = false;
+                foreach (ProductionRule r in rules)
+                {
+                    if (r.pre.Equals(m))
+                    {
+                        if (ApplyRule(r.prob))
+                        {
+                            for (int j = 0; j < r.suc.Count; ++j)
+                            {
+                                float[] paramList = r.suc[j].parameters.ToArray();
+                                Debug.Log(r.suc[j] + "has " + paramList.Length + " params.");
+                                paramList = r.suc[j].trans(paramList);
+                                List<float> ps = new List<float>();
+                                ps.InsertRange(0, paramList);
+                                r.suc[j].SetParams(ps);
+                                nextWord.Add(r.suc[j]);
+                                replaced = true;
+                            }
+                        }
+                    }
+                }
+
+                if (!replaced)
+                    nextWord.Add(m);
+            }
+
+            curWord.Clear();
+            curWord.InsertRange(curWord.Count, nextWord);
+            nextWord.Clear();
+        }
+        return curWord;
+    }
+
+    public void AddRule(Module p, List<Module> s, float prob)
+    {
+        rules.Add(new ProductionRule(p, s, prob));
+    }
+
+    public void AddRule(Module p, List<Module> s)
+    {
+        AddRule(p, s, 1.0f);
+    }
+
+    public void AddRule(char c, List<Module> s)
+    {
+        Module m = new Module(c);
+        AddRule(m, s);
     }
 
     public void AddRule(char p, string s)
@@ -34,47 +164,40 @@ public class LSystem
 
     public void AddRule(char p, string s, float prob)
     {
-        rules.Add(new ProductionRule(p, s, prob));
-    }
+        Module m = new Module(p);
+        List<Module> w = new List<Module>();
 
-    public string RunSystem(int iterations)
-    {
-        string nextWord = "";
-        string curWord = axiom;
-        bool replaced = false;
-
-        for(int i = 0; i < iterations; ++i)
+        foreach (char c in s)
         {
-            foreach(char c in curWord)
-            {
-                replaced = false;
-                foreach(ProductionRule r in rules)
-                {
-                    if(c == r.pre)
-                    {
-                        if (ApplyRule(r.prob))
-                        {
-                            nextWord += r.suc;
-                            replaced = true;
-                            break;
-                        }
-                    }
-                }
-
-                if (!replaced)
-                    nextWord += c;
-            }
-
-            curWord = nextWord;
-            nextWord = "";
+            w.Add(new Module(c));
         }
-
-        return curWord;
+        AddRule(m, w, prob);
     }
 
     private bool ApplyRule(float prob)
     {
         float r = Random.Range(0.0f, 1.0f);
         return r <= prob;
+    }
+
+    private int CountParams(char[] paramList)
+    {
+        int paramCount = 1;
+        foreach(char c in paramList)
+        {
+            if (c == ',')
+                ++paramCount;
+        }
+        return paramCount;
+    }
+
+    public void SetVar(char c, float f)
+    {
+        varMap[c] = f;
+    }
+
+    public float GetVar(char c)
+    {
+        return varMap[c];
     }
 }
--- Assets/Tester.cs
+++ Assets/Tester.cs
@@ -7,10 +7,33 @@ public class Tester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LSystem lSys = new LSystem("F");
-        lSys.AddRule('F', "FF");
+        LSystem.Module a = new LSystem.Module('F', new List<float>{ 1.0f });
+        LSystem lSys = new LSystem(a);
+        lSys.SetVar('c', 1.0f);
+        lSys.SetVar('p', 0.3f);
+        lSys.SetVar('q', lSys.GetVar('c') - lSys.GetVar('p'));
+        lSys.SetVar('h', Mathf.Pow(lSys.GetVar('p') * lSys.GetVar('q'), 0.5f));
 
-        Debug.Log(lSys.RunSystem(4));
+
+        LSystem.Module m = new LSystem.Module('F', new List<float> { 1.0f }, (float[] f) => { f[0] *= lSys.GetVar('p'); return f; });
+        LSystem.Module m1 = new LSystem.Module('F', new List<float> { 1.0f }, (float[] f) => { f[0] *= lSys.GetVar('h'); return f; });
+        LSystem.Module m11 = new LSystem.Module('F', new List<float> { 1.0f }, (float[] f) => { f[0] *= lSys.GetVar('h'); return f; });
+        LSystem.Module m2 = new LSystem.Module('F', new List<float> { 1.0f }, (float[] f) => { f[0] *= lSys.GetVar('q'); return f; });
+
+        LSystem.Module mPlus = new LSystem.Module('+');
+        LSystem.Module mMinus = new LSystem.Module('-');
+        List<LSystem.Module> suc = new List<LSystem.Module> { m, mPlus, m1, mMinus, mMinus, m11,mPlus, m2 };
+        lSys.AddRule(a, suc);
+
+
+        List<LSystem.Module> mods = lSys.RunSystem(1);
+        Debug.Log(mods.Count);
+        string str_out = "";
+        foreach(LSystem.Module mod in mods)
+        {
+            str_out += mod;
+        }
+        Debug.Log(str_out);
     }
 
     // Update is called once per frame
