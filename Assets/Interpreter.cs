cc6be1becd9beb55330fee4b7cd36accedac7f58
ScottSpiers <scott.m.spiers@gmail.com>
2019-06-05 20:44:02 +0100

    Setting up for drawing meshes

---
+++ Assets/Interpreter.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpreter
+{
+    public Interpreter()
+    {
+
+    }
+
+    public MeshFilter InterpretSystem(List<Module> modules, float stepSize, float width, float angleDelta)
+    {
+        TurtleState curState = new TurtleState();
+        TurtleState nextState = new TurtleState();
+
+        Stack<TurtleState> turtleStack = new Stack<TurtleState>();
+        Stack<int> indexStack = new Stack<int>();
+
+        int index = 0;
+
+        foreach(Module m in modules)
+        {
+            nextState = curState;
+            Vector3 rotated = curState.rot.MultiplyVector(Vector3.up);
+            Quaternion q = new Quaternion();
+            
+
+
+            switch (m.sym)
+            {
+                case 'F':
+                    {
+                        nextState.pos += rotated * curState.stepSize; //XMVectorAdd(nextState.pos, XMVectorScale(rotated, curState.stepSize));
+
+                        //Check params: Assume 1st is stepSize 2nd is width 3rd is stepDelta 4th is branchDelta
+                        
+                        //create vertices in cur and next pos, create mid vertex as ((up * width) + cur.pos)
+                        //creating 4 vertices on 1st run 2 after that: the next pos and it's width offset
+                        
+                        //if branchReturn 
+                            //do proper index things
+                        //else
+                            //indices = Ic-2, Ic-1, Ic++
+
+                        break;
+                    }
+                case 'f':
+                    {
+                        nextState.pos += curState.stepSize * rotated;
+                        //THIS IS STILL RELEVANT
+                        //index = m_indices.size(); //need to increase the index, in case we  go back or just so we don't draw where we shouldn't 
+                        break;
+                    }
+                case '+':
+                    {
+                        nextState.RotateAxisAngle(Vector3.forward, angleDelta);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[2], angleDelta);
+                        break;
+                    }
+                case '-':
+                    {
+                        //check for params: 1st is angle, should there be any more?
+                        //change these back to 2!
+                        nextState.RotateAxisAngle(Vector3.forward, -angleDelta);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[2], -angleDelta);
+                        break;
+                    }
+                case '&':
+                    {
+                        nextState.RotateAxisAngle(Vector3.right, angleDelta);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[0], angleDelta);
+                        break;
+                    }
+                case '^':
+                    {
+                        nextState.RotateAxisAngle(Vector3.right, -angleDelta);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[0], -angleDelta);
+                        break;
+                    }
+                case '\\':
+                    {
+                        nextState.RotateAxisAngle(Vector3.up, angleDelta);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[1], angleDelta);
+                        break;
+                    }
+                case '/':
+                    {
+                        nextState.RotateAxisAngle(Vector3.up, angleDelta);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[1], -angleDelta);
+                        break;
+                    }
+                case '|':
+                    {
+                        nextState.RotateAxisAngle(Vector3.forward, 180.0f);
+                        //rotMatrix *= XMMatrixRotationAxis(rotMatrix.r[2], (180.0f * XM_PI) / 180);
+                        break;
+                    }
+                case '[':
+                    {
+                        //nextState.stepSize = curState.stepSize + 1.0f;
+                        //make this a percentage of the curradius?
+                        //nextState.width = curState.width - branchRedDelta;
+                        //if (nextState.radius < 0.05f)
+                        //    nextState.radius = 0.05f;
+                        turtleStack.Push(curState);
+                        indexStack.Push(index);
+                        break;
+                    }
+                case ']':
+                    {
+                        nextState = turtleStack.Pop();
+                        index = indexStack.Pop();
+                        break;
+                    }
+                default:
+                    break;
+            }
+            curState = nextState;
+        }
+        return null;
+    }
+}
---
+++ Assets/TurtleState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TurtleState
+{
+    //The turtle's position
+    public Vector3 pos { get; set; }
+    //The amount the turtle will move in the next step
+    public float stepSize { get; set; }
+    //The width of the ine drawn by the Turtle
+    public float width { get; set; }
+    //The rotation of the turtle
+    public Matrix4x4 rot { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="a"></param>
+    public void RotateAxisAngle(Vector3 axis, float a) // should this also have the axis? Or just pass what we should be changing as we can access it easily
+    {
+        //Update the rotation matrix
+        if(axis != Vector3.up && axis != Vector3.forward && axis != Vector3.right)
+        {
+            return;
+        }
+
+        Vector3 rotation = rot.rotation.eulerAngles;
+        rotation += axis * a;
+        rot = Matrix4x4.Rotate(Quaternion.Euler(rotation));
+    }
+}
